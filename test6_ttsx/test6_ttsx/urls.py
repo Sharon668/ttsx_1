51224f6998a82ff470476b39227cf53dcc3d7f33
Sharon <xxyunx@163.com>
2017-09-09 14:07:43 +0800

    增加用户url

--- test6_ttsx/test6_ttsx/urls.py
+++ test6_ttsx/test6_ttsx/urls.py
@@ -19,4 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'aaaa/',include('booktest.urls')),
+    url(r'user/',include('user.urls')),
+    
 ]
